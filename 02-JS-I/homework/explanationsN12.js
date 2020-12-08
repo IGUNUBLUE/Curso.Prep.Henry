297a7f3c73a0944b7a3419d33fd9093c8dbbdbaf
Lenin Garizabalo <lcamargo@protonmail.com>
2020-12-07 23:00:32 -0500

    Tarea 02-js-1 terminda

---
+++ 02-JS-I/homework/explanationsN12.js
@@ -0,0 +1,21 @@
+function dVariables() {
+    return "La variables son como habitaciones en la que puedes guardar objetos, pero en este caso esos objetos serán datos, información"
+}
+
+function dStrings() {
+    return "Los string, que vendria siendo cadena en español, son letras del alfabeto seguida la una de la otra que forman una frase, palabra y que tiene un valor para el usuario"
+}
+
+function dFunciones() {
+    return "Las funciones son fragmento de codigo que ejecutan una o varias tareas, reciben infomacion a traves de lo argumentos y su respuesta y terminacion esta definada por la palabra return"
+}
+
+function dDeclaraciones() {
+    return "Las declaraciones son pequeñas funciones como definimos anteriormente pero en este caso no sirven para hacer comparaciones que daran como resultado dos opciones; falso o verdero"
+}
+
+function dBoloneanos() {
+    return "la respuestas que nos devuelven las declaraciones son el ejemplo perfecto para mostrar lo que son lo booleanos. basicamente si algo es falso o verdadero."
+}
+
+console.log(dVariables, dStrings, dFunciones, dDeclaraciones, dBoloneanos)
