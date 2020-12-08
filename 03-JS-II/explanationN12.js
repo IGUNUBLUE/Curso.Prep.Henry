628807eb6d80d69b6cb265a15c5984a9e588c87b
Lenin Garizabalo <lcamargo@protonmail.com>
2020-12-08 18:52:00 -0500

    Homework 03-JS-II finished

---
+++ 03-JS-II/explanationN12.js
@@ -0,0 +1,4 @@
+// For: Es un ciclo compuesto por tres argumentos que nos permitirá repetir una verificacion n veces.
+// &&: El operador lógico Y (AND) nos permite comprobar si un argumento cumple con dos o mas requisitos
+// ||: Este operador difiere del anterior porque podemos utilizarlo para comprabar si el argumentos cumple por lo menos con una de las condiciones.
+// !: Con el diferente podemos entregar como respuesta el valor opuesto del booleano de la función o resultado de las operación. 
