d73faba6a583ac6769b56972bb48412ac74c1d4c
Lenin Garizabalo <lcamargo@protonmail.com>
2020-12-08 22:49:27 -0500

    Homework 04-JS-III finished and moving 03-JS-II explanation file

---
+++ 04-JS-III/homework/explanationN12.js
@@ -0,0 +1,4 @@
+// arrays: Imaginemos un array como los hicimos con las variables, supongamos que es un cuarto en el cual se pueden guardar algo.
+// pero en esta caso en ese lugar se pueden almacenar varios tipos de objetos e indentificarlos con algo parecido a una dirreción
+// la cual utilizaremos para encontrarlos cuando los necesitemos. Por ejemplo mi primer objeto (dato), como fue el primero lo pondremos
+// en el espacio 1 (0).
--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -3,16 +3,19 @@
 function devolverPrimerElemento(array) {
   // Devuelve el primer elemento de un array
   // Tu código:
+  return array[0];
 }
 
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
+  return array[array.length - 1];
 }
 
 function obtenerLargoDelArray(array) {
   // Devuelve el largo de un array
   // Tu código:
+  return array.length;
 }
 
 function incrementarPorUno(array) {
@@ -20,12 +23,18 @@ function incrementarPorUno(array) {
   // Aumenta cada entero por 1
   // y devuelve el array
   // Tu código:
+  for (i = 0; i < array.length; i++) {
+    array[i] = array[i] + 1;
+  }
+  return array;
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+  array.push(elemento);
+  return array;
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
@@ -33,6 +42,8 @@ function agregarItemAlComienzoDelArray(array, elemento) {
   // y devuelve el array
   // Pista: usa el método `.unshift`
   // Tu código:
+  array.unshift(elemento);
+  return array;
 }
 
 function dePalabrasAFrase(palabras) {
@@ -41,30 +52,61 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
+  var fullString = ""; 
+  var lastWord = palabras[palabras.length - 1]; // guardando la ultima palabra ya esta no necesita espacio al final.
+  for (i = 0; i < palabras.length - 1; i++) { // recorriendo el array hasta el penultimo indice que sera el último con espacio.
+    fullString = fullString + palabras[i] + " "; // concatenando todos los elementos en una variable y agregando espacio al final de cada palabra.
+  }
+  fullString = fullString + lastWord; // agregando el último elemento de la array, no necesita tener espacio al final.
+  return fullString;
 }
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for (i = 0; i < array.length; i++) {
+    if (elemento === array[i]) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function agregarNumeros(numeros) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var addNumber = 0; 
+  for (i = 0; i < numeros.length; i++) {
+    addNumber = addNumber + numeros[i];
+  }
+  return addNumber;
 }
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  var addScore = 0; 
+  for (i = 0; i < resultadosTest.length; i++) {
+    addScore = addScore + resultadosTest[i];
+  }
+  promedio = addScore / resultadosTest.length;
+  return promedio;
 }
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var bigNum = 0;
+  for (i = 0; i < numeros.length; i++) {
+    if (numeros[i] > bigNum) {
+      bigNum = numeros[i];
+    }
+  }
+  return bigNum;
 }
 
 function multiplicarArgumentos() {
@@ -72,6 +114,19 @@ function multiplicarArgumentos() {
   // Si no se pasan argumentos devuelve 0
   // Si se pasa un argumento, simplemente devuélvelo
   // Tu código:
+  var resultMulti = 1;
+  if (arguments.length === 0) {
+    return 0;
+  }
+  else if (arguments.length === 1) {
+    return arguments[0];
+  }
+  else {
+    for (i = 0; i < arguments.length; i++) {
+      resultMulti = resultMulti * arguments[i];  
+    }
+  }
+  return resultMulti;
 }
 
 // No modificar nada debajo de esta línea
