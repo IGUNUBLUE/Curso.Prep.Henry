d5f1a5bbe8261a7763d8325931ea140ad8d2a88f
Lenin Garizabalo <lcamargo@protonmail.com>
2020-12-17 20:09:23 -0500

    Homework 06-JS-V finished updated

---
+++ 06-JS-V/homework/explanationN12.js
@@ -0,0 +1,7 @@
+/*
+
+Prototype: Mi primera conclusión es que los prototipos son la base que tienes los objetos en JS, dependiendo del objeto siempre habrá un padre al cual recurrir.
+
+Constructores o clases (class): son plantillas personalisadas que se pueden crear para unificar propiedades o metodos, y asi no repetirse o gastar recursos innesesarios.
+
+*/
